2262b74fc112e8cf7bc0b2c9e1e9851422f7d615
shivathebravo <shivathebravo@gmail.com>
2019-09-06 15:18:39 -0400

    2 files modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,43 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { OutletComponent } from './outlet/outlet.component';
+import { LoginComponent } from './login/login.component';
+import { ResetComponent } from './reset/reset.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductService } from './product.service';
+import { DetailsComponent } from './details/details.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+
+const appRoutes:Routes=[
+{path: '',redirectTo:'login',pathMatch:'full'},
+{path: 'login',component:LoginComponent},
+{path: 'reset-password',component:ResetComponent},
+{path:'**',component:NotFoundComponent}
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserSettingsFormComponent
+    UserSettingsFormComponent,
+    LoginComponent,
+    ResetComponent,
+    OutletComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    InstructionsComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
